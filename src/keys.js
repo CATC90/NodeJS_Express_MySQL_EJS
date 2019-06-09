412f7f96f48f7ca078cf767e6b4cf27a499d7350
Carlos <catc_90@hotmail.com>
2019-06-08 22:50:54 -0400

    minor changes

--- src/keys.js
+++ src/keys.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
     database: {
         host: 'localhost',
-        //        port: '3306',
         user: 'root',
         password: 'admin',
         database: 'videoJuegos'
