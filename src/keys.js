9662e0797c89cb056068ea1070ec536698138c1f
Carlos <catc_90@hotmail.com>
2019-06-08 22:55:40 -0400

    minor changes

--- src/keys.js
+++ src/keys.js
@@ -4,7 +4,7 @@ module.exports = {
     database: {
         host: 'localhost',
         user: 'root',
-        password: 'admin',
+        password: '',
         database: 'videoJuegos'
     },
     home: {
